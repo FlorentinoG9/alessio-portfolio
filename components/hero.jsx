6fa59242ff629d60d7cab8f4b0fbf9c43ab58e1e
FlorentinoG9 <f.valle.garcia@gmail.com>
2021-12-20 14:06:55 -0600

    Hero section setup

--- components/hero.jsx
+++ components/hero.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from 'react';
 
 export default function Hero() {
-  return (
-    <div>
-      Hero
-    </div>
-  )
+	return (
+		<section id='hero' className='max-w-md mx-auto text-white scroll-mt-24'>
+			<div>
+				<h2 className='text-xl font-bold'>My Portfolio</h2>
+				<p>
+					Lorem ipsum, dolor sit amet consectetur adipisicing elit. Assumenda,
+					exercitationem adipisci deleniti ad ipsa accusantium hic! Ex natus
+					culpa a!
+				</p>
+			</div>
+		</section>
+	);
 }
