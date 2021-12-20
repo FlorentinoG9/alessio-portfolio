fee7fcc1bddaab9e979852d35ead58491aaa0a60
FlorentinoG9 <f.valle.garcia@gmail.com>
2021-12-19 23:05:07 -0600

    update to tailwind v3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,15 @@
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {}
-  },
-  variants: {
-    extend: {}
-  },
-  plugins: []
-}
+	purge: [
+		'./pages/**/*.{js,ts,jsx,tsx}',
+		'./components/**/*.{js,ts,jsx,tsx}',
+		'./layout/**/*.{js,ts,jsx,tsx}',
+	],
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		extend: {},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
+};
